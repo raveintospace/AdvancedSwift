9e0be696b2a0dd7df0596c6174c426649651ec0c
Uri <uriaeri@gmail.com>
2023-10-29 11:38:34 +0100

    Lesson 11, pt 4 - Second variant of tabBarVersion, has shadow & matching geometry effects

--- SwiftfulAdvance/CustomTabBar/CustomTabBarView.swift
+++ SwiftfulAdvance/CustomTabBar/CustomTabBarView.swift
@@ -11,18 +11,11 @@ struct CustomTabBarView: View {
     
     let tabs: [TabBarItem]
     @Binding var selection: TabBarItem
+    @Namespace private var namespace
     
     var body: some View {
-        HStack {
-            ForEach(tabs, id: \.self) { tab in
-                tabView(tab: tab)
-                    .onTapGesture {
-                        switchToTab(tab: tab)
-                    }
-            }
-        }
-        .padding(6)
-        .background(Color.white.ignoresSafeArea(edges: .bottom))
+        // tabBarVersion1
+        tabBarVersion2
     }
 }
 
@@ -55,9 +48,61 @@ extension CustomTabBarView {
         .cornerRadius(10)
     }
     
+    private var tabBarVersion1: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabView(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(Color.white.ignoresSafeArea(edges: .bottom))
+    }
+    
     private func switchToTab(tab: TabBarItem) {
         withAnimation(.easeInOut) {
             selection = tab
         }
+    }
+}
+
+// matching geometry effect & shadow effect
+extension CustomTabBarView {
+    private func tabView2(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .foregroundColor(selection == tab ? tab.color : Color.gray)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                if selection == tab {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(tab.color.opacity(0.2))
+                        .matchedGeometryEffect(id: "background_rectangle", in: namespace)
+                }
+            }
+        )
+    }
+    
+    private var tabBarVersion2: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabView2(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(Color.white.ignoresSafeArea(edges: .bottom))
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 5)
+        .padding(.horizontal)
     }
 }
